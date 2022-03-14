de01190cae771341d1c7a0af7ea971336ae9041d
Gabriel de Araujo de Souza <96799743+GbAraujoSouza@users.noreply.github.com>
2022-03-14 11:09:53 -0300

    Update funcoes_utilizadas.py

--- funcoes_utilizadas.py
+++ funcoes_utilizadas.py
@@ -1,7 +1,8 @@
+from turtle import pos
 import numpy as np
 import random as rd
 
-def crossover(cromossomo1: list, cromossomo2: list, probabilidadeCruzamento: float) -> list:
+def crossover(cromossomo1: np.array, cromossomo2: np.array, probabilidadeCruzamento: float) -> np.array and np.array:
 
     # Verificar se ocorrerá o cruzamento 
     # Cria-se um numero aleatório "na" de 0 a 1.
@@ -17,4 +18,17 @@ def crossover(cromossomo1: list, cromossomo2: list, probabilidadeCruzamento: flo
         return novoCromossomo1,novoCromossomo2
     else:
         # Não realizar cruzamento e retornar cromossomos originais
-        return novoCromossomo1,novoCromossomo2
+        return cromossomo1,cromossomo2
+
+def mutacao(cromossomo: np.array, probabilidadeMutacao: float) -> np.array:
+    # Criar "chance" de mutação 
+    na = rd.random()
+    if na < probabilidadeMutacao:
+        # Criar posição aleatória
+        posAleatoria = rd.randint(0,len(cromossomo)-1)
+        # realizar mutação
+        cromossomo[posAleatoria] = abs(cromossomo[posAleatoria]-1)
+        return cromossomo
+    else:
+        return cromossomo
+    
